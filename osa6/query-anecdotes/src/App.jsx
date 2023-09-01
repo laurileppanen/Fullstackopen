9dec8097ab8f801d525060f625ccb58387677b31
Lauri Leppänen <late-22-@hotmail.com>
2023-09-01 09:52:30 +0300

    6.20

---
+++ osa6/query-anecdotes/src/App.jsx
@@ -0,0 +1,48 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+
+const App = () => {
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => getAnecdotes(),
+    retry: 3
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  if ( result.isLoading ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
