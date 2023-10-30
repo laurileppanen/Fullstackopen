078349d17bd806b0a1313f1fcf5f707053b936bb
Lauri Leppänen <late-22-@hotmail.com>
2023-10-30 13:43:09 +0200

    9.9

--- osa9/patientor/patientor-backend/index.ts
+++ osa9/patientor/patientor-backend/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
-const app = express();
-app.use(express.json());
 
-const PORT = 3000;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
