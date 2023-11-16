5167c6f393305777e54346fe13f4fa33f62d6f42
Lauri Leppänen <late-22-@hotmail.com>
2023-11-16 16:31:16 +0200

    9.17

--- osa9/flight-diary/flight-diary-frontend/src/App.tsx
+++ osa9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import { Diary } from "./types";
-import { getAllDiaries } from "./services/diaryService";
+import { getAllDiaries, createDiary } from "./services/diaryService";
 
 const App = () => {
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     getAllDiaries().then((data) => {
@@ -11,9 +15,53 @@ const App = () => {
     });
   }, []);
 
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createDiary({
+      date: date,
+      weather: weather,
+      visibility: visibility,
+      comment: comment,
+    }).then((data) => {
+      setDiaries(diaries.concat(data));
+    });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
+
   return (
     <div>
+      <h2>Add new entry</h2>
+
+      <form onSubmit={diaryCreation}>
+        date
+        <input value={date} onChange={(event) => setDate(event.target.value)} />
+        <br />
+        visibility
+        <input
+          value={visibility}
+          onChange={(event) => setVisibility(event.target.value)}
+        />
+        <br />
+        weather
+        <input
+          value={weather}
+          onChange={(event) => setWeather(event.target.value)}
+        />
+        <br />
+        comment
+        <input
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <br />
+        <button type="submit">add</button>
+      </form>
+
       <h2>Diary Entries</h2>
+
       {diaries.map((diary) => (
         <div key={diary.id}>
           <h3>{diary.date}</h3>
