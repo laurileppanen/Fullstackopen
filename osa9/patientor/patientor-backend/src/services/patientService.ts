71aabe083e74106bdb26a3ed9337f7bf7c9563b4
Lauri Leppänen <late-22-@hotmail.com>
2024-01-29 17:41:28 +0200

    9.22

--- osa9/patientor/patientor-backend/src/services/patientService.ts
+++ osa9/patientor/patientor-backend/src/services/patientService.ts
@@ -1,4 +1,4 @@
-import patients from "../../data/patients";
+import patients from "../../data/newPatients";
 import {
   PatientEntry,
   NonSensitivePatientEntry,
--- osa9/patientor/patientor-backend/src/types.ts
+++ osa9/patientor/patientor-backend/src/types.ts
@@ -4,18 +4,14 @@ export interface DiagnoseEntry {
   latin?: string;
 }
 
-export interface Entry {
-  entry: [];
-}
-
 export interface PatientEntry {
-  id: string;
   name: string;
-  dateOfBirth: string;
   ssn: string;
-  gender: string;
   occupation: string;
+  dateOfBirth: string;
+  gender: string;
   entries: Entry[];
+  id: string;
 }
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, "entries" | "ssn">;
@@ -27,3 +23,45 @@ export enum Gender {
   Female = "female",
   Other = "other",
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
